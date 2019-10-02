b384074626f5556dbf378a319343288529b1a5b0
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-02 16:15:26 -0400

    Created Supper Fragment

---
+++ app/src/main/java/com/remijonathan/foodmenu/SupperFragment.java
@@ -0,0 +1,28 @@
+package com.remijonathan.foodmenu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SupperFragment extends Fragment {
+
+    public SupperFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_supper, container, false);
+    }
+}
